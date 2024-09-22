b1e7b2258873f6473c7da76ee6e94aaefad44dd3
mszahan <mszahan1@gmail.com>
2024-09-22 18:57:26 +0600

    added models for product gallery and variations

--- shop/models.py
+++ shop/models.py
@@ -45,3 +45,23 @@ class Product(models.Model):
         return reverse('shop:product_detail', args=[self.id, self.slug])
 
 
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, related_name='variations', on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+    stock = models.PositiveIntegerField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class Gallery(models.Model):
+    product = models.ForeignKey(Product, related_name='galleries', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='porducts/%Y/%m', blank=True)
+    name = models.CharField(max_length=150, blank=True, null=True)
+    order = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ['order']
+        verbose_name = 'gallery'
+        verbose_name_plural = 'galleries'
