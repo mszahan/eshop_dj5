e627956b502e8d4ccf95a657c3c3f0971f3a3770
mszahan <mszahan1@gmail.com>
2024-10-21 16:11:37 +0600

    activated add to cart button in product detail page

--- cart/forms.py
+++ cart/forms.py
@@ -6,6 +6,6 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 6)]
 
 class CartAddProductForm(forms.Form):
     quantity = forms.TypedChoiceField(
-        choices=PRODUCT_QUANTITY_CHOICES, coerce=int
+        choices=PRODUCT_QUANTITY_CHOICES, coerce=int,initial=1
     )
     override = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
--- cart/urls.py
+++ cart/urls.py
@@ -5,6 +5,6 @@ app_name = 'cart'
 
 urlpatterns = [
     path('', views.cart_detail, name='cart_detail'),
-    path('add/<int:variation_id', views.cart_add, name='cart_add'),
+    path('add/<int:variation_id>', views.cart_add, name='cart_add'),
     path('remove/<int:variation_id>', views.cart_remove, name='cart_remove'),
 ]
