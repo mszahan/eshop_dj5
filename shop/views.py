ff9dc977d0ee2190e0631a974a2e32f35d020211
mszahan <mszahan1@gmail.com>
2024-10-21 20:09:25 +0600

    added cart to product list page

--- shop/views.py
+++ shop/views.py
@@ -7,14 +7,15 @@ def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
-    # cart_product_form = CartAddProductForm()
+    cart_product_form = CartAddProductForm()
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
     return render(request, 'shop/product/list.html', {
         'category': category, 
         'categories': categories, 
-        'products': products,})
+        'products': products,
+        'cart_product_form': cart_product_form,})
 
 
 
